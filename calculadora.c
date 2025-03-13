958da3fbf3f671e996c2e86b1ba2c467379f8da2
henao1f <jjhb2007@gmail.com>
2025-03-13 14:26:35 +0000

    correcion calculadora

--- calculadora.c
+++ calculadora.c
@@ -2,10 +2,11 @@
 
 int main() {
     int opcion;
-    float muneroA, numeroB, resultado;
+    float numeroA, numeroB, resultado;
 
     printf("Ingrese el primer número: ");
     scanf("%f", &numeroA);
+    
     printf("Ingrese el segundo número: ");
     scanf("%f", &numeroB);
 
@@ -22,27 +23,25 @@ int main() {
             resultado = numeroA + numeroB;
             printf("\nResultado: %.2f + %.2f = %.2f\n", numeroA, numeroB, resultado);
             break;
-
         case 2:
-            resultado = numeroA - numneroB;
-            printf("\nResultado: %.2f - %.2f = %.2f\n ", numeroA, numeroB, resultado);
+            resultado = numeroA - numeroB;
+            printf("\nResultado: %.2f - %.2f = %.2f\n", numeroA, numeroB, resultado);
             break;
-
         case 3: 
             resultado = numeroA * numeroB;
             printf("\nResultado: %.2f * %.2f = %.2f\n", numeroA, numeroB, resultado);
             break;
-
         case 4:
-            if (numeroB !=0) {
-                Resultado = numeroA / numeroB;
+            if (numeroB != 0) {
+                resultado = numeroA / numeroB;
                 printf("\nResultado: %.2f / %.2f = %.2f\n", numeroA, numeroB, resultado);
             } else {
-                printf("Error:No se puede dividir entre cero.\n");
+                printf("\nError: No se puede dividir entre cero.\n");
             }
+            break;
+        default:
+            printf("\nOpción no válida. Vuelve a intentarlo.\n");
     }
-    break;
-   default;
-        printf("\nOpcion no valida. vuelve a intentarlo.\n");
+
     return 0;
 }
